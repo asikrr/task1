acc81a0479c7cc885d4fdbcde5bd95c566ecbcd5
Anastasiia <asav32048@gmail.com>
2025-11-10 18:42:28 +0700

    Создание главной страницы

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Book, Author, BookInstance, Genre
+
+def index(request):
+    num_books=Book.objects.all().count()
+    num_instances=BookInstance.objects.all().count()
+
+    num_instances_available=BookInstance.objects.filter(status__exact='a').count()
+    num_authors=Author.objects.count()
+
+    search_for_book = 'book1'
+    search_for_genre = 'fantasy'
+
+    num_books_contain_search = Book.objects.filter(title__icontains=search_for_book).count()
+    num_genres_contain_search = Genre.objects.filter(name__icontains=search_for_genre).count()
+
+    return render(
+        request,
+        'index.html',
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,
+                 'num_authors':num_authors,'search_for_book':search_for_book,'search_for_genre':search_for_genre,
+                 'num_books_contain_search':num_books_contain_search, 'num_genres_contain_search':num_genres_contain_search},
+    )
