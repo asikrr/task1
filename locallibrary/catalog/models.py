ecc328ad7bda4dec61d9cdd166480be1666600ef
asikrr <asav32048@gmail.com>
2025-11-11 13:32:23 +0700

    Создание страницы для книг, которые заимствовал пользователь

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from django.urls import reverse
 from django.db.models import UniqueConstraint
 from django.db.models.functions import Lower
+from datetime import date
 import uuid
+from django.contrib.auth.models import User
 
 class Genre(models.Model):
     name = models.CharField(max_length=200, help_text="Enter a book genre (e.g. Science Fiction, French Poetry etc.)")
@@ -71,6 +73,13 @@ class BookInstance(models.Model):
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
+    borrower = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
@@ -83,10 +92,11 @@ class BookInstance(models.Model):
 
     class Meta:
         ordering = ["due_back"]
-
+        permissions = (("can_mark_returned", "Set book as returned"),)
 
     def __str__(self):
-        return '%s (%s)' % (self.id,self.book.title)
+        return f'{self.id} ({self.book.title})'
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
