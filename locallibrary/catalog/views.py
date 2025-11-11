78e10795c456238d4434975db9e68d2fc01bfc1e
Anastasiia <asav32048@gmail.com>
2025-11-11 20:33:38 +0700

    Обновлена форма для книги, добавлена ссылка на страницу с изменением даты возврата книги

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 import datetime
 
-from .forms import RenewBookForm
+from .forms import RenewBookModelForm
 
 def index(request):
     num_books=Book.objects.all().count()
@@ -63,17 +63,17 @@ def renew_book_librarian(request, pk):
 
     if request.method == 'POST':
 
-        form = RenewBookForm(request.POST)
+        form = RenewBookModelForm(request.POST)
 
         if form.is_valid():
-            book_inst.due_back = form.cleaned_data['renewal_date']
+            book_inst.due_back = form.cleaned_data['due_back']
             book_inst.save()
 
-            return HttpResponseRedirect(reverse('all-borrowed') )
+            return HttpResponseRedirect(reverse('all-borrowed-books') )
 
     else:
         proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
-        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date,})
+        form = RenewBookModelForm(initial={'due_back': proposed_renewal_date})
 
     return render(request, 'catalog/book_renew_librarian.html', {'form': form, 'bookinst':book_inst})
 
