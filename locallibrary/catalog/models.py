7d30d889f444e9752853352ca33abba18e8fe3b1
Anastasiia <asav32048@gmail.com>
2025-11-11 23:33:47 +0700

    Изменения модели автора: обновление verbose_name для даты смерти, обновление def __str__

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -103,7 +103,7 @@ class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
-    date_of_death = models.DateField('Died', null=True, blank=True)
+    date_of_death = models.DateField('died', null=True, blank=True)
 
     def clean(self):
         super().clean()
@@ -132,7 +132,7 @@ class Author(models.Model):
 
 
     def __str__(self):
-        return '%s, %s' % (self.last_name, self.first_name)
+        return f'{self.last_name}, {self.first_name}'
 
     class Meta:
         ordering = ['last_name']
